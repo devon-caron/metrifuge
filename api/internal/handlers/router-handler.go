25428aecd93cd168759ddf284cfda669c5fa2290
Devon Caron <devon.caron.p@gmail.com>
2025-09-27 11:27:51 -0700

    commenting out to allow refactor, will revamp

--- api/internal/handlers/router-handler.go
+++ api/internal/handlers/router-handler.go
@@ -1,20 +1,18 @@
 package handlers
 
 import (
-	"github.com/devon-caron/metrifuge/receiver"
 	"github.com/go-chi/chi"
-	chimiddle "github.com/go-chi/chi/middleware"
 )
 
 func RouterHandler(router *chi.Mux) {
 	// Global Log Receiver
-	lr := receiver.GetLogReceiver()
+	// lr := receiver.GetLogReceiver()
 
-	// Global middleware
-	router.Use(chimiddle.StripSlashes)
+	// // Global middleware
+	// router.Use(chimiddle.StripSlashes)
 
-	router.Route("/api", func(router chi.Router) {
-		router.Get("/health", lr.HealthHandler)
-		router.Post("/ingest", lr.ReceiveLogsHandler)
-	})
+	// router.Route("/api", func(router chi.Router) {
+	// 	router.Get("/health", lr.HealthHandler)
+	// 	router.Post("/ingest", lr.ReceiveLogsHandler)
+	// })
 }
