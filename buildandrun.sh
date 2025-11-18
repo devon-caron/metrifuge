591565742133d7652a8fd0bb6a247e0386eaad0c
Devon Caron <devon.caron.p@gmail.com>
2025-11-17 23:18:52 -0700

    qol for build script

--- buildandrun.sh
+++ buildandrun.sh
@@ -1,12 +1,17 @@
+echo "==== Building..."
 ./build.sh
-kubectl delete po/metrifuge-test -n metrifuge --force
-sleep 1
+
+echo "==== Deleting old pods..."
+kubectl delete po/metrifuge-test -n metrifuge
+kubectl delete po/mf-log-pod -n metrifuge
+
+echo "==== Loading image..."
 mftagtbl=$(docker images --format "table {{.Repository}}\t{{.Tag}}\t{{.CreatedAt}}" | head -n 2)
 mftagname=$(echo "$mftagtbl" | grep metrifuge | grep -v "latest" | awk '{print $1}')
 mftagtime=$(echo "$mftagtbl" | grep metrifuge | grep -v "latest" | awk '{print $2}')
 minikube image load "$mftagname:$mftagtime"
+
+echo "==== Updating pods..."
 yq eval --inplace ".spec.containers[0].image = \"$mftagname:$mftagtime\"" mf-pod.yaml
-kubectl delete po -n metrifuge mf-log-pod --force
-sleep 1
 kubectl apply -f mf-log-pod.yaml
 kubectl apply -f mf-pod.yaml
